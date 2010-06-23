2b41188c80bb0c6b5f7dfb2acabc0014445a5963
Viktor Kelemen <viktor@cirius.co.jp>
2010-06-23 23:57:37 +0900

    Huge refactoring, actually making it work

--- result.js
+++ result.js
@@ -1,21 +1,55 @@
 
 
-function createHTML(content) {
-    
-    
-    var container = document.createElement("div");
-    
-    container.setAttribute("id", "qgoogle-result");
+function createHTML(entries) {
+
+    var container = document.getElementById("qgoogle-result");
+
+    if (container === null) {
+        container = document.createElement("div");
+        container.setAttribute("id", "qgoogle-result");
+        document.body.appendChild(container);
+    }
+
+
+    var content = "";
+
+    for (var i = 0, l = entries.length; i < l; i++) {
+        content += "<h3>" + entries[i].title + "</h3>";
+        content += "<p>" + entries[i].desc + "</p>";
+        content += "<hr>";
+    }
     container.innerHTML = content;
-    
-    document.body.appendChild(container);
-    
+
+
+
 }
 
+function clearHTML() {
+
+    var node = document.getElementById("qgoogle-result");
+    if (node !== undefined) {
+        document.body.removeChild(node);
+    }
+}
+
+
+function processMessage(message) {
+
+
+    if (message.verb === "update") {
+        createHTML(message.entries);
+        return;
+    }
+
+    if (message.verb === "clear") {
+        clearHTML();
+        return;
+    }
+}
 
 chrome.extension.onRequest.addListener(
   function(request, sender, sendResponse) {
 
-    createHTML(request.htmlContent);
+    processMessage(request);
   }
 );
